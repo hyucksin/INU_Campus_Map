15131653686e1c1def270aab3970ece4d40f4c3a
Hyucksin Kwon <hyicksin@gmail.com>
2014-12-03 23:30:47 +0900

    레이아웃 변경 및 이미지 크기 조절

--- src/com/example/inucampusmap/BuildingInfoActivity0.java
+++ src/com/example/inucampusmap/BuildingInfoActivity0.java
@@ -1,46 +1,232 @@
 package com.example.inucampusmap;
 
+import java.io.*;
 import java.util.*;
+
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMultiplayer.InitiateMatchResult;
+
 import android.app.*;
 import android.content.*;
 import android.content.res.*;
+import android.graphics.*;
 import android.graphics.drawable.*;
 import android.os.*;
 import android.view.*;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.webkit.*;
 import android.widget.*;
 
 public class BuildingInfoActivity0 extends Activity{
 	ImageView mImageView;
+	Bitmap bitmap = null;
+	TextView textView = null;
+	TextRoom textRoom = new TextRoom();
+	ArrayList<String> textArray = new TextRoom().textArray;
+	
+	private LinearLayout mImage;
+	private LinearLayout mText;
+	
+	final int F_ONE = 0;
+    final int F_TWO = 1;
+    final int F_THREE = 2;
+    final int F_FOUR = 3;
+    final int F_FIVE = 4;
+    
+    final int R_ONE = 5;
+    final int R_TWO = 6;
+    final int R_THREE = 7;
+    final int R_FOUR = 8;
+    final int R_FIVE = 9;
+	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building_info0);
+        
         mImageView = (ImageView)findViewById(R.id.buildingImageView);
-    }
-	
-	
+        textView = (TextView)findViewById(R.id.textRoomList);
+        		
+        Button mapButton = (Button)findViewById(R.id.floorMapButton);
+        Button roomButton = (Button)findViewById(R.id.floorRoomButton);
+        
+        registerForContextMenu(mapButton);
+        registerForContextMenu(roomButton);
+        
+        mImage = (LinearLayout)findViewById(R.id.frameLayoutImage);
+    	mText = (LinearLayout)findViewById(R.id.frameLayoutText);
+        
+	}
 	//선택한 버튼에 따라 해당 Activity로 이동
     public void onClick(View v) {
     	Resources res;
-    	BitmapDrawable bitmap;
+    	Bitmap bitmap;
     	switch(v.getId()){
-    	
-    	//누른 버튼에 따라 이미지를 다르게 띄움
-    	case R.id.f1Button :
-    		res = getResources();
-    		bitmap = (BitmapDrawable) res.getDrawable(R.drawable.b7_1);
-    		mImageView.setImageDrawable(bitmap);
-    		break;
-    	case R.id.f2Button :
-    		res = getResources();
-    		bitmap = (BitmapDrawable) res.getDrawable(R.drawable.b7_2);
-    		mImageView.setImageDrawable(bitmap);
-    		break;
     	case R.id.locationButton :      		
     		res = getResources();
-    		bitmap = (BitmapDrawable) res.getDrawable(R.drawable.b7_3);
-    		mImageView.setImageDrawable(bitmap);
+    		bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.b7_1);
+        	bitmap = Bitmap.createScaledBitmap(bitmap, 450, 600, true);
+        	mImageView.setImageBitmap(bitmap);
+            mImageView.setScaleType(ImageView.ScaleType.FIT_START);
+    		break;
+    	case R.id.floorMapButton :
+    		this.openContextMenu(v);
+    		break;
+    	case R.id.floorRoomButton :
+    		this.openContextMenu(v);
     		break;
     	}
     }
+    
+    @Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		
+			if(v.getId() == R.id.floorMapButton) {
+				menu.setHeaderIcon(android.R.drawable.btn_star);
+				menu.setHeaderTitle("층별 구조도 보기");
+				menu.add(Menu.NONE, F_ONE, Menu.NONE, "1층");
+				menu.add(Menu.NONE, F_TWO, Menu.NONE, "2층");
+				menu.add(Menu.NONE, F_THREE, Menu.NONE, "3층");
+				menu.add(Menu.NONE, F_FOUR, Menu.NONE, "4층");
+				menu.add(Menu.NONE, F_FIVE, Menu.NONE, "5층");
+			} 
+			else if(v.getId() == R.id.floorRoomButton){				
+				menu.setHeaderIcon(android.R.drawable.btn_star);
+				menu.setHeaderTitle("층별 방 목록 보기");
+				menu.add(Menu.NONE, R_ONE, Menu.NONE, "1층");
+				menu.add(Menu.NONE, R_TWO, Menu.NONE, "2층");
+				menu.add(Menu.NONE, R_THREE, Menu.NONE, "3층");
+				menu.add(Menu.NONE, R_FOUR, Menu.NONE, "4층");
+				menu.add(Menu.NONE, R_FIVE, Menu.NONE, "5층");
+			}
+		
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		
+		switch (item.getItemId()) {
+		case F_ONE:
+			mImage.setVisibility(LinearLayout.VISIBLE);
+            mText.setVisibility(LinearLayout.INVISIBLE);
+			bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.bf_7_1);
+        	bitmap = Bitmap.createScaledBitmap(bitmap, 450, 600, true);
+        	mImageView.setImageBitmap(bitmap);
+            mImageView.setScaleType(ImageView.ScaleType.FIT_START);
+			break;
+		case F_TWO:
+			mImage.setVisibility(LinearLayout.VISIBLE);
+            mText.setVisibility(LinearLayout.INVISIBLE);
+			bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.bf_7_2);
+        	bitmap = Bitmap.createScaledBitmap(bitmap, 450, 600, true);
+        	mImageView.setImageBitmap(bitmap);
+            mImageView.setScaleType(ImageView.ScaleType.FIT_START);
+			break;
+		case F_THREE:
+			mImage.setVisibility(LinearLayout.VISIBLE);
+            mText.setVisibility(LinearLayout.INVISIBLE);
+			bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.bf_7_3);
+        	bitmap = Bitmap.createScaledBitmap(bitmap, 450, 600, true);
+        	mImageView.setImageBitmap(bitmap);
+            mImageView.setScaleType(ImageView.ScaleType.FIT_START);
+			break;
+		case F_FOUR:
+			mImage.setVisibility(LinearLayout.VISIBLE);
+            mText.setVisibility(LinearLayout.INVISIBLE);
+			bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.bf_7_4);
+        	bitmap = Bitmap.createScaledBitmap(bitmap, 450, 600, true);
+        	mImageView.setImageBitmap(bitmap);
+            mImageView.setScaleType(ImageView.ScaleType.FIT_START);
+			break;
+		case F_FIVE:
+			mImage.setVisibility(LinearLayout.VISIBLE);
+            mText.setVisibility(LinearLayout.INVISIBLE);
+			bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.bf_7_5);
+        	bitmap = Bitmap.createScaledBitmap(bitmap, 450, 600, true);
+        	mImageView.setImageBitmap(bitmap);
+            mImageView.setScaleType(ImageView.ScaleType.FIT_START);
+			break;
+			
+		case R_ONE:
+			mImage.setVisibility(LinearLayout.INVISIBLE);
+            mText.setVisibility(LinearLayout.VISIBLE);
+            textView.setText(textArray.get(0));
+			break;
+		case R_TWO:
+			mImage.setVisibility(LinearLayout.INVISIBLE);
+            mText.setVisibility(LinearLayout.VISIBLE);
+            textView.setText(textArray.get(1));
+			break;
+		case R_THREE:
+			mImage.setVisibility(LinearLayout.INVISIBLE);
+            mText.setVisibility(LinearLayout.VISIBLE);
+			break;
+		case R_FOUR:
+			mImage.setVisibility(LinearLayout.INVISIBLE);
+            mText.setVisibility(LinearLayout.VISIBLE);
+			break;
+			
+		case R_FIVE:
+			mImage.setVisibility(LinearLayout.INVISIBLE);
+            mText.setVisibility(LinearLayout.VISIBLE);
+			break;
+
+		default:
+			break;
+		}
+		
+		return super.onContextItemSelected(item);
+	}
+}
+
+class TextRoom {
+	ArrayList<String> textArray;
+	TextRoom(){
+		textArray = new ArrayList<String>();
+		textArray.add("101 : 디지털실험실\n" +
+				"102 : 기초정보공학실험실\n" +
+				"103 : 단과대학생회실\n" +
+				"104 : 강의실(중)-1\n" +
+				"105 : 정보시스템실험실\n" +
+				"106 : PC검색실\n" +
+				"107 : 정보통신실험실\n" +
+				"108 : 학과학생회실\n" +
+				"109 : 경비실\n" +
+				"110 : 집중구내통신실\n" +
+				"111 : 컴퓨터실험실-1\n" +
+				"112 : 여학생휴게실\n" +
+				"113 : 샤워실(여)\n" +
+				"114 : 탈의실(여)\n" +
+				"115 : 샤워실(남)\n" +
+				"116 : 탈의실(남)");
+		textArray.add("201 : HCI연구실\n" +
+				"202 : 분산네트워크연구실\n" +
+				"203 : 무선정보연구실\n" +
+				"204 : 강사대기실\n" +
+				"205 : 정보처리실험실\n" +
+				"206 : 정보공학실험실\n" +
+				"209 : 학과사무실\n" +
+				"210 : 실험준비실-2\n" +
+				"211 : 컴퓨터실험실-2\n" +
+				"212 : 실험준비실-1\n" +
+				"213 : 임베디드영상신호처리실험실\n" +
+				"214 : 교수실험실\n" +
+				"215 : 전파공학전파환경연구실\n" +
+				"216 : 통신망및보안연구실\n" +
+				"217 : 유비쿼터스네트워크연구실\n" +
+				"218 : 교수연구실\n" +
+				"219 : 교수연구실\n" +
+				"220 : 교수연구실\n" +
+				"221 : 교수연구실\n" +
+				"222 : 교수연구실\n" +
+				"223 : 교수연구실\n" +
+				"224 : 교수연구실\n" +
+				"225 : 교수연구실\n" +
+				"226 : 교수연구실\n" +
+				"227 : 교수연구실\n" +
+				"228 : 교수연구실\n" +
+				"229 : 교수연구실\n");
+
+	}
 }
--- src/com/example/inucampusmap/UseInfoActivity0.java
+++ src/com/example/inucampusmap/UseInfoActivity0.java
@@ -20,98 +20,104 @@ public class UseInfoActivity0 extends Activity{
         mImageView = (ImageView)findViewById(R.id.useInfoImageView);
         mImageView2 = (ImageView)findViewById(R.id.useInfoMapImage);
         
-        Resources res = getResources();
-        BitmapDrawable bitmap = null;
-        BitmapDrawable bitmap2 = null;
-        
-        
-        
+        Bitmap bitmap = null;
+        Bitmap bitmap2 = null;
         
         switch(infoNumber)
         {
 	        case 0 :
-	        	bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i0);
-	        	bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im0);break;
+	        	//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i0);
+	        	//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im0);break;
+	        	bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.i0);
+	        	bitmap = Bitmap.createScaledBitmap(bitmap, 450, 300, true);
+	        	bitmap2 = BitmapFactory.decodeResource(getResources(),R.drawable.im0);
+	        	bitmap2 = Bitmap.createScaledBitmap(bitmap2, 450, 300, true);
+	        	
 			case 1 :
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i1);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im1);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i1);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im1);break;
 			case 2 :
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i2);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im2);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i2);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im2);break;
 			case 3 :
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i3);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im3);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i3);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im3);break;
 			case 4 :
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i4);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im4);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i4);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im4);break;
 			case 5 :
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i5);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im5);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i5);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im5);break;
 			case 6 :
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i6);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im6);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i6);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im6);break;
 			case 7 :
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i7);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im7);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i7);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im7);break;
 			case 8 :
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i8);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im8);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i8);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im8);break;
 			case 9 :
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i9);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im9);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i9);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im9);break;
 			case 10:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i10);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im10);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i10);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im10);break;
 			case 11:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i11);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im11);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i11);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im11);break;
 			case 12:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i12);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im12);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i12);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im12);break;
 			case 13:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i13);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im13);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i13);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im13);break;
 			case 14:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i14);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im14);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i14);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im14);break;
 			case 15:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i15);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im15);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i15);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im15);break;
 			case 16:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i16);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im16);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i16);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im16);break;
 			case 17:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i17);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im17);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i17);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im17);break;
 			case 18:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i18);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im18);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i18);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im18);break;
 			case 19:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i19);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im19);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i19);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im19);break;
 			case 20:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i20);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im20);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i20);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im20);break;
 			case 21:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i21);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im21);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i21);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im21);break;
 			case 22:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i22);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im22);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i22);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im22);break;
 			case 23:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i23);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im23);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i23);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im23);break;
 			case 24:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i24);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im24);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i24);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im24);break;
 			case 25:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i25);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im25);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i25);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im25);break;
 			case 26:
-				bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i26);
-				bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im26);break;
+				//bitmap = (BitmapDrawable)res.getDrawable(R.drawable.i26);
+				//bitmap2 = (BitmapDrawable)res.getDrawable(R.drawable.im26);break;
         }
-        mImageView.setImageDrawable(bitmap);
-        mImageView2.setImageDrawable(bitmap2);
+        //mImageView.setImageDrawable(bitmap);
+        //mImageView2.setImageDrawable(bitmap2);
+        mImageView.setImageBitmap(bitmap);
+        mImageView.setScaleType(ImageView.ScaleType.FIT_START);
+        mImageView2.setImageBitmap(bitmap2);
+        mImageView2.setScaleType(ImageView.ScaleType.FIT_START);
+        
     }
 }
