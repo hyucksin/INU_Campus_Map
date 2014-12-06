5b829cb762a795fd5c3deb1dbc646dcd610be9bd
Sanghoon Kim <ksh070794@nate.com>
2014-12-06 16:53:01 +0900

    건물정보 7호관 오류 수정 및 완성

--- src/com/example/inucampusmap/BuildingInfoActivity0.java
+++ src/com/example/inucampusmap/BuildingInfoActivity0.java
@@ -61,7 +61,7 @@ public class BuildingInfoActivity0 extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_building_info0);
 		
-		mImageView = (ImageView)findViewById(R.id.buildingImageView);
+		
 		textView = (TextView)findViewById(R.id.textRoomList);
 
 		Button mapButton = (Button)findViewById(R.id.floorMapButton);
@@ -79,11 +79,17 @@ public class BuildingInfoActivity0 extends Activity{
 		Bitmap bitmap;
 		switch(v.getId()){
 		case R.id.locationButton :
-		
 			setContentView(R.layout.activity_building_mapshow);
+			bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.b7);
+        	bitmap = Bitmap.createScaledBitmap(bitmap, 450, 300, true);
+        	mImageView = (ImageView)findViewById(R.id.buildingImageView);
+        	mImageView.setImageBitmap(bitmap);
+            mImageView.setScaleType(ImageView.ScaleType.FIT_START);
+			loc = new LatLng(37.374494, 126.633517);
+          	cp = new CameraPosition.Builder().target((loc)).zoom(17).build();
 			mMap=((MapFragment)getFragmentManager().findFragmentById(R.id.campus_mapshow)).getMap();
 			mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp));   
-			marker = mMap.addMarker(new MarkerOptions().position(new LatLng(37.37449,126.63349)));
+			marker = mMap.addMarker(new MarkerOptions().position(loc));
 			break;
 		case R.id.floorMapButton :
 			this.openContextMenu(v);
@@ -103,9 +109,9 @@ public class BuildingInfoActivity0 extends Activity{
 			menu.setHeaderTitle("층별 구조도 보기");
 			menu.add(Menu.NONE, F_ONE, Menu.NONE, "1층");
 			menu.add(Menu.NONE, F_TWO, Menu.NONE, "2층");
-			menu.add(Menu.NONE, F_THREE, Menu.NONE, "3층");
+			menu.add(Menu.NONE,F_THREE, Menu.NONE, "3층");
 			menu.add(Menu.NONE, F_FOUR, Menu.NONE, "4층");
-			menu.add(Menu.NONE, F_FIVE, Menu.NONE, "5층");
+			menu.add(Menu.NONE,F_FIVE, Menu.NONE, "5층");
 		} 
 		else if(v.getId() == R.id.floorRoomButton){				
 			menu.setHeaderIcon(android.R.drawable.btn_star);
@@ -114,7 +120,7 @@ public class BuildingInfoActivity0 extends Activity{
 			menu.add(Menu.NONE, R_TWO, Menu.NONE, "2층");
 			menu.add(Menu.NONE, R_THREE, Menu.NONE, "3층");
 			menu.add(Menu.NONE, R_FOUR, Menu.NONE, "4층");
-			menu.add(Menu.NONE, R_FIVE, Menu.NONE, "5층");
+			menu.add(Menu.NONE,R_FIVE, Menu.NONE, "5층");
 		}
 
 		super.onCreateContextMenu(menu, v, menuInfo);
@@ -178,15 +184,17 @@ public class BuildingInfoActivity0 extends Activity{
 		case R_THREE:
 			mImage.setVisibility(LinearLayout.INVISIBLE);
 			mText.setVisibility(LinearLayout.VISIBLE);
+			textView.setText(textArray.get(2));
 			break;
 		case R_FOUR:
 			mImage.setVisibility(LinearLayout.INVISIBLE);
 			mText.setVisibility(LinearLayout.VISIBLE);
+			textView.setText(textArray.get(3));
 			break;
-
 		case R_FIVE:
 			mImage.setVisibility(LinearLayout.INVISIBLE);
 			mText.setVisibility(LinearLayout.VISIBLE);
+			textView.setText(textArray.get(4));
 			break;
 
 		default:
@@ -244,6 +252,74 @@ class TextRoom {
 				"227 : 교수연구실\n" +
 				"228 : 교수연구실\n" +
 				"229 : 교수연구실\n");
-
+		textArray.add("301 지능로봇시스템실험실\n"+
+				"302 캡스턴디자인실습실\n"+
+				"303 학과학생회실-2\n"+
+				"304 강희실(대)-계단식\n"+
+				"305 프로그래밍실습실\n"+
+				"306 임베디드SW실습실\n"+
+				"309 학과사무실\n"+
+				"310 실험준비실\n"+
+				"311 입베디드시스템실험실\n"+
+				"312 임베디드네트워크시스템 설계연구실\n"+
+				"313 강의실(중)-2\n"+
+				"314 강의실(소)-1\n"+
+				"315 ABEEK자료실\n"+
+				"316 창의설계실험실\n"+
+				"317 임베디드HW실습실\n"+
+				"318 임베디드SW설계실험실\n"+
+				"319 모바일앱실험실\n"+
+				"320 교학실\n"+
+				"321 교수회의실\n"+
+				"322 학장실\n"+
+				"323~328 교수연구실\n"+
+				"329 임베디드시스템구조실험실\n"+
+				"330 무선통신실험실\n");
+		textArray.add("401 모바일컴퓨팅연구실\n"+
+				"402 무선정보네트워크연구실\n"+
+				"403 세미나실\n"+
+				"404 기자재실\n"+
+				"405 정보통신대학원학생회실\n"+
+				"406 강의실(소)-2\n"+
+				"407 강의실 (중)-3\n"+
+				"408 컴퓨터실습실-1\n"+
+				"409 실습준비실\n"+
+				"410 학부사무실\n"+
+				"413 멀티미디어가상현실연구실\n"+
+				"414 인터넷소프트웨어연구실\n"+
+				"415 컴퓨터실습실-2\n"+
+				"416 세미나실(대)\n"+
+				"417 강의실(소)-3\n"+
+				"418 첨단SW연구센터\n"+
+				"419 컴퓨터공학부 학생회실(주)\n"+
+				"420 프로젝트실습실\n"+
+				"421 엔터테인먼트컴퓨팅연구실\n"+
+				"422 컴퓨터그래픽스연구실\n"+
+				"423 분산시스템연구실\n"+
+				"424 Enion연구실\n"+
+				"425~434 교수연구실\n"+
+				"435 데이터베이스연구실\n"+
+				"436 지능소프트웨어연구실\n");
+		textArray.add("501 컴퓨터실습실3\n"+
+				"502 HW실습실\n"+
+				"503 컴퓨터실\n"+
+				"504 강의실(중)-1\n"+
+				"505 강의실(중)-2\n"+
+				"506 IMPRESS연구실\n"+
+				"509 교수연구실\n"+
+				"509-1 교수연구실\n"+
+				"510 학부학생회실\n"+
+				"511 컴퓨터실습실-4\n"+
+				"512서버실\n"+
+				"513 컴퓨터공학과학생회실(야)\n"+
+				"514 프로젝트실습실-2\n"+
+				"515 단과대학학생회실\n"+
+				"516 연구실\n"+
+				"517 학과사무실\n"+
+				"517-1 학과학생회실\n"+
+				"518 초빙교수실\n"+
+				"519~524 공동교수실\n"+
+				"525~527 교수연구실\n"+
+				"528 시스템스프트웨어연구실\n");
 	}
 }
